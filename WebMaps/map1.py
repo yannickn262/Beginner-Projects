a60f17c9bf2e0eefbcdb02519491218593fc01b7
Yannick Nandury <37671464+yannickn262@users.noreply.github.com>
2018-12-25 16:22:38 -0500

    Adds Layer Control for Population/Volcanoes
    Adds toggle for layer control.

--- WebMaps/map1.py
+++ WebMaps/map1.py
@@ -21,15 +21,19 @@ Volcano name:<br>
 Height: %s m
 """
 map = folium.Map(location=[40.7146, 69], tiles = "Mapbox Bright")
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt,ln, el, name in zip(lat,lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location=[lt,ln], radius = 6, popup=folium.Popup(iframe), fill_color = color_producer(el), color = 'grey', fill_opacity = 0.7))
+    fgv.add_child(folium.CircleMarker(location=[lt,ln], radius = 6, popup=folium.Popup(iframe), fill_color = color_producer(el), color = 'grey', fill_opacity = 0.7))
 
-fg.add_child(folium.GeoJson(data = open('world.json', 'r', encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data = open('world.json', 'r', encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 #map.save("Map1.html")
 map.save("Map_html_popup_advanced.html")
