9bc867fed8050810586b6253f705c8a58bb7c7d0
Yannick Nandury <37671464+yannickn262@users.noreply.github.com>
2018-12-25 16:15:31 -0500

    Adds Polygonal Layer in Map1
    Also changes markers to circles with gray border. Adds lambda function that allows customization of values based on geoJson Data.

--- WebMaps/map1.py
+++ WebMaps/map1.py
@@ -6,6 +6,15 @@ lon = list(data["LON"])
 elev = list(data["ELEV"])
 name = list(data["NAME"])
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'blue'
+    else:
+        return 'red'
+
+
 html = """
 Volcano name:<br>
 <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
@@ -16,7 +25,11 @@ fg = folium.FeatureGroup(name="My Map")
 
 for lt,ln, el, name in zip(lat,lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt,ln], popup=folium.Popup(iframe), icon = folium.Icon(color='green')))
+    fg.add_child(folium.CircleMarker(location=[lt,ln], radius = 6, popup=folium.Popup(iframe), fill_color = color_producer(el), color = 'grey', fill_opacity = 0.7))
+
+fg.add_child(folium.GeoJson(data = open('world.json', 'r', encoding='utf-8-sig').read(), style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
 map.add_child(fg)
 #map.save("Map1.html")
 map.save("Map_html_popup_advanced.html")
