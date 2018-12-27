81604a072d4f1872765b781967fd0af78cc56304
Yannick Nandury <37671464+yannickn262@users.noreply.github.com>
2018-12-26 19:43:37 -0500

    Creates Website Blocker

---
+++ Block_Websites/website_blocker.py
@@ -0,0 +1,28 @@
+import time
+from datetime import datetime as dt
+
+hosts_temp = "hosts"
+hosts_path = "/etc/hosts"
+redirect = "127.0.0.1"
+website_list =["www.facebook.com", "facebook.com", "instagram.com", "www.instagram.com"]
+
+while True:
+    #blocks social media websites between 9am and 5pm
+    if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, 17):
+        print("Working hours...")
+        with open(hosts_path, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+    else:
+        with open(hosts_path, 'r+') as file:
+            content = file.readlines()
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("Fun hours...") #hours of the day where access to social media is allowed
+    time.sleep(5)
