42c5a57d28c67afbc434bb53c19a16bdb575e9e3
Yannick Nandury <37671464+yannickn262@users.noreply.github.com>
2019-06-19 11:35:02 -0400

    fixed pandas csv bug

--- realEstate.py
+++ realEstate.py
@@ -8,32 +8,33 @@ c = r.content
 soup = BeautifulSoup(c, "html.parser")
 all = soup.find_all("div", {"class": "detail-wrap"})
 
-hash = {}
+
 l = []
 for item in all:
+    hash = {}
     #finds price
     try:
         hash["Price"] = item.find("span", {"class": "data-price"}).text.replace("\n", "").replace(" ", "")
-        print (hash["Price"])
+        #print (hash["Price"])
     except:
         hash["Price"] = "None"
     #finds # beds and prevents print from auto creating a newline
     try:
         hash["Beds"] = item.find("span", {"class": "data-value meta-beds"}).text.replace(" ", "")
-        print (hash["Beds"] + " bed ", end = " ")
+        #print (hash["Beds"] + " bed ", end = " ")
     except:
         hash["Beds"] = "None"
     try:
         #attempting to get the number of bathrooms
         hash["Baths"] = item.find("ul", {"class": "prop-meta ellipsis"}).find_all("li")[1].find("span",{"class":"data-value"}).text.replace(" ", "")
-        print(hash["Baths"] + " bath ", end = " ")
+        #print(hash["Baths"] + " bath ", end = " ")
     except:
         hash["Baths"] = "None"
         pass
     try:
         #gets square footage
         hash["Sqft"] = item.find("ul", {"class": "prop-meta ellipsis"}).find_all("li")[2].find("span",{"class":"data-value"}).text.replace(" ", "")
-        print(hash["Sqft"] + " sqft ")
+        #print(hash["Sqft"] + " sqft ")
     except:
         hash["Sqft"] = "None"
         pass
@@ -42,11 +43,11 @@ for item in all:
         str = item.find("div", {"class" : "address ellipsis "}).text.replace("\n", "").replace(" ", "")
         hash["Address"] = str
         #need to fix: add space to first part of address and last (can solve via more detailed scrape or use regex commands)
-        print(hash["Address"])
+        #print(hash["Address"])
     except:
         hash["Address"] = "none"
     l.append(hash)
-    print()
+    #print()
 
 df = pandas.DataFrame(l)
 df.to_csv("Output.csv")
